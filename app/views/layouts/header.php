b987ae8265f8a3ddd6864af581157b50bc1ec534
Penguinn35 <cong3535c@gmail.com>
2025-11-30 09:37:58 +0700

    resolve homepage bug due to old version overrided

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -5,6 +5,15 @@
     <meta charset="UTF-8">
     <title>Laptop Store Việt Nam</title>
     <link rel="stylesheet" href="/laptop_store/public/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+    <!-- CSS riêng cho từng trang -->
+    <?php if (!empty($pageCss)): ?>
+        <link rel="stylesheet" href="/laptop_store/public/css/<?= $pageCss ?>.css">
+    <?php endif; ?>
+
+
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 </head>
